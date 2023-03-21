08249c8ff2731848a7fce8534e71a39df85da5d0
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-21 21:26:35 +0600

    data fetch and state changr

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 
 import './App.css'
 
@@ -7,9 +7,29 @@ function App() {
 
   return (
    <div className="app">
-    hi
+    <LoadCountries/>
    </div>
   )
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/all`)
+  .then(res=>res.json())
+  .then(data=>setCountries(data))
+  }, [])
+  
+  return(
+    <div>
+      <h1>Visiting Every Countries</h1>
+      <h3>Availavale countries:{countries.length}</h3>
+      {/* {
+        countries.map(country=>)
+      } */}
+    </div>
+  )
+  
 }
 
 export default App
