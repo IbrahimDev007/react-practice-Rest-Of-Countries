4387a584de291e1446e33fd621386f75664653ec
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-22 13:39:09 +0600

    resolved

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -6,11 +6,11 @@ export default function Country({countries}) {
   return (
     
     <div className="card  col-4 g-4">
-    <img src={flags.png} className="card-img-top" />
+    <img src={flags.png} className="card-img-top p-4 mx-2" />
     <div className="card-body">
-      <h5 className="card-title">{name.common}</h5>
+      <h5 className="card-title">Name:{name.common}</h5>
       <p className="card-text">Area:{area}</p>
-      <p className="card-text">{cca2}</p>
+      <p className="card-text">Shortname:{cca2}</p>
     </div>
   </div>
   )
